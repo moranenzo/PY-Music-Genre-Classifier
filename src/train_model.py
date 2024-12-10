463b39ece7cabae46bc18da9d25c3e0fac5dece5
moranenzo <enzo.moran337@gmail.com>
2024-12-10 14:32:00 +0000

    Update save_model to also save KGB, LGB and RF models

--- src/train_model.py
+++ src/train_model.py
@@ -1,8 +1,13 @@
 # Required imports
-from catboost import CatBoostClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, classification_report
 from df_downloader import get_df
+from utils import save_model
+
+from catboost import CatBoostClassifier
+from sklearn.ensemble import RandomForestClassifier
+import xgboost as xgb
+import lightgbm as lgb
 
 df_path = "/tlaflotte/genre_detector/spotify_tracks.csv"
 
@@ -27,15 +32,23 @@ def preprocess_data(df):
     return train_test_split(X, y, test_size=0.2, random_state=42)
 
 
-# Train the model
-def train_model(X_train, y_train):
-    model = CatBoostClassifier(
-        iterations=500, 
-        learning_rate=0.1, 
-        depth=8, 
-        loss_function='MultiClass', 
+# Dummy CatBoost model
+cat_model = CatBoostClassifier(
+        iterations=500,
+        learning_rate=0.1,
+        depth=8,
+        loss_function='MultiClass',
         verbose=100
     )
+
+# Dummy Models
+rf_model = RandomForestClassifier()
+xgb_model = xgb.XGBClassifier()
+lgb_model = lgb.LGBMClassifier()
+
+
+# Train the model
+def train_model(model, X_train, y_train):
     model.fit(X_train, y_train)
     return model
 
@@ -48,16 +61,12 @@ def evaluate_model(model, X_test, y_test):
     print(classification_report(y_test, y_pred))
     return accuracy
 
-# Save the model
-def save_model(model, model_path="models/music_genre_model.cbm"):
-    model.save_model(model_path)
-    print(f"Model saved at {model_path}")
 
 # Main function
 if __name__ == "__main__":
     df = load_data(df_path)
     if df is not None:
         X_train, X_test, y_train, y_test = preprocess_data(df)
-        model = train_model(X_train, y_train)
+        model = train_model(cat_model, X_train, y_train)
         evaluate_model(model, X_test, y_test)
-        save_model(model)
+        save_model(model, model_name="catboost")
