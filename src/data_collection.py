00d5b5224d201c0758e600bbc3e157fa725b7095
moranenzo <enzo.moran337@gmail.com>
2024-12-29 22:06:18 +0000

    Add playlist_ids

--- src/data_collection.py
+++ src/data_collection.py
@@ -1,113 +1,112 @@
+# Importation des librairies
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 import csv
 import time
 
 
-
-# Spotify client initialization
+# Initialisation du client Spotify
 def spotify_client():
     """
-    Initialize the Spotify API client with client credentials.
-    Returns an authenticated Spotify client.
+    Initialise le client de l'API Spotify avec les identifiants du client.
+    Retourne un client Spotify authentifié.
     """
     return spotipy.Spotify(auth_manager=SpotifyClientCredentials(
         client_id="d666ee3ae4c94b85945c3dba39776f4f",
-        client_secret="c1973a77acbe48c0b2f105e4f57d7d46" 
+        client_secret="c1973a77acbe48c0b2f105e4f57d7d46"
     ))
 
 
-# Fetch tracks from the playlist
+# Récupérer les morceaux d'une playlist
 def fetch_playlist_tracks(playlist_id):
     """
-    Fetch all tracks from a Spotify playlist. 
-    Params:
-        -playlist_id: Spotify playlist ID.
-    Returns a list of dictionaries containing track details.
+    Récupère tous les morceaux d'une playlist Spotify.
+    Paramètres :
+        - playlist_id : ID de la playlist Spotify.
+    Retourne une liste de dictionnaires contenant les détails des morceaux.
     """
     tracks = []
     results = spotify_client().playlist_tracks(playlist_id)
     while results:
         for item in results['items']:
             track = item['track']
-            if track:  # Ensure the track is not None
+            if track:  # S'assurer que le morceau n'est pas None
                 tracks.append(track)
         results = spotify_client().next(results) if results['next'] else None
     return tracks
 
 
-# Fetch metadata from tracks
+# Récupérer les métadonnées des morceaux
 def fetch_track_data(tracks):
     """
-    Fetch metadata and audio features for each track in the playlist.
-    Params:
-        -tracks: List of tracks from the playlist.
-    Returns a list of dictionaries containing track metadata and audio features.
+    Récupère les métadonnées et les caractéristiques audio pour chaque morceau de la playlist.
+    Paramètres :
+        - tracks : Liste des morceaux de la playlist.
+    Retourne une liste de dictionnaires contenant les métadonnées des morceaux et leurs caractéristiques audio.
     """
     track_data = []
-    i=0
-    j=0
+    i = 0
+    j = 0
     for track in tracks:
         track_id = track['id']
-        i+=1
+        i += 1
         print(i)
         audio_features = spotify_client().audio_features([track_id])[0]
-        genre=fetch_artist_genre(track)
-        if audio_features:  # Ensure audio features are available
+        genre = fetch_artist_genre(track)
+        if audio_features:  # S'assurer que les caractéristiques audio sont disponibles
             artist_name = ", ".join([artist['name'] for artist in track['artists']])
-            dict_track={"track Name": track['name'],
-                "artists": artist_name,
-                "track_id": track_id,
-                "popularity": track['popularity'],
-                "duration_ms": track['duration_ms'],
-                "explicit": track['explicit'], 'genre': genre}
+            dict_track = {"track Name": track['name'],
+                          "artists": artist_name,
+                          "track_id": track_id,
+                          "popularity": track['popularity'],
+                          "duration_ms": track['duration_ms'],
+                          "explicit": track['explicit'], 'genre': genre}
             for key in audio_features.keys():
-                dict_track[key]=audio_features[key]
+                dict_track[key] = audio_features[key]
             track_data.append(dict_track)
-        if i==100:
-            j+=1
+        if i == 100:
+            j += 1
             save_to_csv(track_data, f"intermédiaire{j}")
-            i=0
+            i = 0
             time.sleep(60)
     return track_data
 
 
-# Fetch the genre
+# Récupérer le genre de l'artiste
 def fetch_artist_genre(track):
-    """This function fetches the genre of an artist with a track from this artist, it will be 
-    considerated as the genre of the song later
+    """Cette fonction récupère le genre d'un artiste à partir d'un morceau de cet artiste,
+    il sera ensuite considéré comme le genre de la chanson.
 
-    Args:
-        track a dict the countains infos about the track
+    Arguments :
+        track un dict contenant des informations sur le morceau
 
-    Returns: genre a string that is the genre of an artist
-
+    Retourne : genre un string représentant le genre de l'artiste
     """
-    artist=track['artists'][0]['id']
+    artist = track['artists'][0]['id']
     if spotify_client().artist(artist)['genres'] != []:
         return spotify_client().artist(artist)['genres'][0]
     else:
         return 'N/A'
 
 
-# Save the df
+# Sauvegarder dans un fichier CSV
 def save_to_csv(data, filename):
     """
-    Save the list of track data to a CSV file.
-    Params:
-    :param data: List of dictionaries containing track details.
-    :param filename: Output CSV file name.
+    Sauvegarde la liste des données des morceaux dans un fichier CSV.
+    Paramètres :
+    :param data : Liste de dictionnaires contenant les détails des morceaux.
+    :param filename : Nom du fichier CSV de sortie.
     """
     keys = data[0].keys() if data else []
     with open(filename, mode='w', newline='', encoding='utf-8') as file:
         writer = csv.DictWriter(file, fieldnames=keys)
         writer.writeheader()
         writer.writerows(data)
-    print(f"Data saved to {filename}")
+    print(f"Données sauvegardées dans {filename}")
 
 
 if __name__ == "__main__":
-    playlist_ids = [['1G8IpkZKobrIlXcVPoSIuf','7gqtGYFoCR3tAqTtEUQZTw']]
+    playlist_ids = ['1G8IpkZKobrIlXcVPoSIuf', '7gqtGYFoCR3tAqTtEUQZTw']
     track_data = []
     for playlist in playlist_ids:
         tracks = fetch_playlist_tracks(playlist)
